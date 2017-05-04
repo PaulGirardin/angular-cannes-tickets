356ce47abc414cb88c2fa1d9518db544f4ec8e0a
Paul Girardin <paul.girardin@eemi.com>
2017-05-04 19:03:46 +0200

    final

---
+++ app.js
@@ -0,0 +1,70 @@
+var app = angular.module("cannes-ticket", ["ngTouch", "mm.foundation", "ngRoute"]);
+
+app.config(['$routeProvider', function($routeProvider){
+  $routeProvider.
+  when('/', {
+        templateUrl: 'templates/index.html',
+        controller: "FrontController",
+        controllerAs: 'ctrl',
+        activetab: 'home'
+      })
+}]);
+
+app.run(['$rootScope', '$route', function($rootScope, $route) {
+	$rootScope.$route = $route;
+	$rootScope.$on('$routeChangeSuccess', function () {
+	});
+}])
+
+app.controller('FrontController', 
+  ['$scope', '$http', '$q',
+    function($scope, $http, $q){
+    var ctrl= this;
+    ctrl.data = null;
+    
+    var promListing = $http.get("raw-data.txt");
+    promListing.then(function(response){
+      var loadedData =  response.data.split("\r\n\r\n");
+      var dataArray = [];
+
+      loadedData.forEach(function (data) {
+          var littleArray = data.split("\r\n");
+          var finalArray = [];
+
+          littleArray.forEach(function (d, index) {
+            if (index > 2) {
+              finalArray.push(d.split(','));
+            } else {
+              finalArray.push(d);
+            }
+          });
+
+          dataArray.push(finalArray);
+      });
+
+      ctrl.data = dataArray;
+      // If you want to dl a json
+      // var obj = JSON.parse(JSON.stringify(dataArray));
+      // var blob = new Blob([obj], {type: "application/json"});
+      // var url  = URL.createObjectURL(blob);
+      // var a = document.createElement('a');
+      // a.download    = "backup.json";
+      // a.href        = url;
+      // a.textContent = "Download backup.json";
+
+      // document.getElementById('content').appendChild(a);
+    });
+
+
+    $q.all([promListing])
+      .then(function(){
+                console.log("everything is loaded !");
+        }, function(){
+                console.error("OOPS !")
+            });
+
+    // ctrl.placeMarker = function(map, position){
+    //   ctrl.marker.setPosition(position);
+    //   ctrl.guess = position;
+    // }
+}]);
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" ng-app="cannes-ticket">
+<head>
+	<meta charset="UTF-8">
+	<title>RBNB</title>
+	<script src="bower_components/jquery/dist/jquery.js"></script>
+	<script src="bower_components/angular/angular.js"></script>
+	<script src="bower_components/angular-foundation-6/dist/angular-foundation.min.js"></script>
+	<script src="bower_components/angular-touch/angular-touch.js"></script>
+	<script src="bower_components/angular-route/angular-route.js"></script>
+	<script src="bower_components/foundation-sites/dist/js/foundation.min.js"></script>
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" />
+	<link rel="stylesheet" href="./app.css" />
+
+	<script src="app.js"></script>
+</head>
+<body>
+<div class="container">
+	<div id="content"></div>
+	<div ng-view></div>
+</div>
+</body>
+</html>
